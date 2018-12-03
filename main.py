9f2ebdc7b89b1482273752235365cb8ab2f1cd44
hundredrab <sourab.jha@kgec.edu.in>
2018-12-03 19:15:36 +0530

    PEP8 and some updates (what?)

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
+from PIL import Image
+from kivy.uix.label import Label
+import zbar
+import os
+import time
+from kivy.clock import Clock
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.properties import ObjectProperty
+from kivy.uix.scatterlayout import ScatterLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from kivy.app import App
 import kivy
 kivy.require('1.10.0')
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.scatterlayout import ScatterLayout
-from kivy.properties import ObjectProperty
-from kivy.uix.tabbedpanel import TabbedPanel
-from kivy.clock import Clock
-import time
-import os
 # import cv2
 # import android
-import zbar
-from kivy.uix.label import Label
-from PIL import Image
 
 Builder.load_string('''
 <cameraClick>:
@@ -31,23 +31,6 @@ Builder.load_string('''
                     resolution: (640, 480)
                     play: True
                 
-                BoxLayout:
-                    id: boxy
-                    size_hint_x: None
-                    width:'48dp'
-                    orientation: 'vertical'
-                    ToggleButton:
-                        orientation: 'vertical'
-                        id: toggle
-                        text: 'Play'
-                        on_press: camera.play = not camera.play
-                        size_hint_y: None
-                        height:'48dp'
-                    Button:
-                        text: 'Capture'
-                        size_hint_y: None
-                        height: '48dp'
-                        on_press: root.capture()
         Screen:
             id: sc2
             name: 'gallery'
@@ -81,8 +64,8 @@ Builder.load_string('''
 
 
 class CameraClick(TabbedPanel):
-    
     manager = ObjectProperty(None)
+
     def switch_to(self, header):
         self.manager.current = header.screen
         self.current_tab.state = "normal"
@@ -95,39 +78,6 @@ class CameraClick(TabbedPanel):
         else:
             self.ids.camera.play = False
         self.ids.file_chooser.path = App.get_running_app().user_data_dir
-    def capture(self):
-        camera = self.ids['camera']
-        scanner = zbar.ImageScanner()
-        scanner.parse_config('enable')
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        # app_folder = os.path.dirname(os.path.abspath(__file__))
-        # print(app_folder)
-        tc=App.get_running_app()
-        print(tc.user_data_dir, '---------------------------')
-        app_folder = tc.user_data_dir
-        if not os.path.exists(app_folder):
-            os.makedirs(app_folder)
-        camera.export_to_png(app_folder+"/IMG_{}.jpg".format(timestr))
-        print(os.listdir(app_folder))
-        pil = Image.open(app_folder+"/IMG_{}.jpg".format(timestr)).convert('L')
-        width, height = pil.size
-        try:
-            raw = pil.tostring()
-        except( AttributeError, NotImplementedError):
-            raw = pil.tobytes()
-        image = zbar.Image(width, height, 'Y800', raw)
-
-        scanner.scan(image)
-
-        print("Captured!")
-        for symbol in image:
-            print 'decoded', symbol.type, symbol.data
-            label = self.ids.toggle
-            label.text = str(symbol.type) + ' ' + str(symbol.data)
-            label = self.ids.result
-            label.text = str(symbol.type) + '\n\n' + str(symbol.data)
-            break
-        del(image)
 
     def decode(self, *args):
         try:
@@ -135,7 +85,7 @@ class CameraClick(TabbedPanel):
             width, height = pil.size
             try:
                 raw = pil.tostring()
-            except( AttributeError, NotImplementedError):
+            except(AttributeError, NotImplementedError):
                 raw = pil.tobytes()
             image = zbar.Image(width, height, 'Y800', raw)
             scanner = zbar.ImageScanner()
@@ -145,15 +95,14 @@ class CameraClick(TabbedPanel):
             print("Captured!")
             for symbol in image:
                 print 'decoded', symbol.type, symbol.data
-                label = self.ids.toggle
-                label.text = str(symbol.type) + ' ' + str(symbol.data)
                 label = self.ids.result
                 label.text = str(symbol.type) + '\n\n' + str(symbol.data)
+                self.switch_to(self.ids.sc3head)
                 break
             del(image)
         except Exception as ae:
             print(ae)
-    
+
     def scheduled_scan(self, dt):
         camera = self.ids['camera']
         scanner = zbar.ImageScanner()
@@ -161,28 +110,23 @@ class CameraClick(TabbedPanel):
         timestr = time.strftime("%Y%m%d_%H%M%S")
         # app_folder = os.path.dirname(os.path.abspath(__file__))
         # print(app_folder)
-        tc=App.get_running_app()
-        print(tc.user_data_dir, '---------------------------')
+        tc = App.get_running_app()
         app_folder = tc.user_data_dir
         if not os.path.exists(app_folder):
             os.makedirs(app_folder)
         camera.export_to_png(app_folder+"/IMG_{}.jpg".format(timestr))
-        print(os.listdir(app_folder))
         pil = Image.open(app_folder+"/IMG_{}.jpg".format(timestr)).convert('L')
         width, height = pil.size
         try:
             raw = pil.tostring()
-        except( AttributeError, NotImplementedError):
+        except(AttributeError, NotImplementedError):
             raw = pil.tobytes()
         image = zbar.Image(width, height, 'Y800', raw)
 
         scanner.scan(image)
 
-        print("Captured!")
         for symbol in image:
             print 'decoded', symbol.type, symbol.data
-            label = self.ids.toggle
-            label.text = str(symbol.type) + ' ' + str(symbol.data)
             label = self.ids.result
             label.text = str(symbol.type) + '\n\n' + str(symbol.data)
             self.switch_to(self.ids.sc3head)
@@ -191,7 +135,7 @@ class CameraClick(TabbedPanel):
         else:
             os.remove(app_folder+"/IMG_{}.jpg".format(timestr))
             print("Found nothing.")
-            Clock.schedule_once(self.scheduled_scan, 1.0/60.0)
+            Clock.schedule_once(self.scheduled_scan, 1.0/2)
         del(image)
 
 
@@ -201,9 +145,5 @@ class SQRReader(App):
         Clock.schedule_once(cam.scheduled_scan, 1.0/60.0)
         return cam
 
-# class MyApp(App):
-#     def build(self):
-#         return Label(text="hello world")
 
-# MyApp().run()
 SQRReader().run()
